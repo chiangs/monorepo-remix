c7df17c9a7e2e5596d7d6aee4fc4de1aa87c4840
Stephen Chiang <stephen.chiang@webstep.no>
2022-08-24 11:54:02 +0200

    [Test] - feature: Setup Vitest for UI-Vite package

---
+++ packages/ui-vite/src/Button/snapshot.test.tsx
@@ -0,0 +1,11 @@
+import renderer from 'react-test-renderer';
+import { describe, expect, test } from 'vitest';
+import Button from './index';
+
+describe('Button', () => {
+    test('Button component renders correctly', () => {
+        const component = renderer.create(<Button />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
